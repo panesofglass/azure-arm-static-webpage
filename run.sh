074bf063d95e91820308a65ca47d8c74d46184e5
Ryan Riley <ryanriley@live.com>
2020-06-01 22:49:10 -0500

    Minimum viable deploy script to create static hosting and load .zip of content

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-az extension add --name storage-preview
-az storage blob service-properties update --static-website --404-document error.html --index-document index.html
-
-az storage blob upload-batch -s /mnt/content/dist -d "\$web"
